f2e66491e13f25b2c326291d245c08fd047476c0
doniatekaya <166394585+doniatekaya@users.noreply.github.com>
2025-03-05 00:53:28 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,8 @@
 import os
 
-PROJECT_ID = "tidal-mason-451914-p4"
+PROJECT_ID = "my-dproject-452220"
+INSTANCE = "elyessiki"
 REGION = "europe-west1"
-DB_PASSWORD = "Donia12345."  # 🔒 Récupère le mot de passe depuis .env
-
-# Cloud SQL
-INSTANCE = "chatbot"
-DATABASE = "donia_db"
-TABLE_NAME = "doni_table"
+DATABASE = "health_database"
 DB_USER = "postgres"
-
-# Cloud Storage
-BUCKET_NAME = "fichier-bucket"
-TABLE_NAME = "doni_table"
+TABLE_NAME = "elyes_med"
